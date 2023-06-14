cfea516f95b99a37a043fed9320a9714b85a2f5d
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-06-14 16:51:17 -0400

    Try using "-e"

--- src/hipscat_import/pipeline.py
+++ src/hipscat_import/pipeline.py
@@ -35,7 +35,7 @@ def pipeline_with_client(args: RuntimeArguments, client: Client):
     """
     try:
         if not args:
-            raise ValueError("args is required and should be subclass RuntimeArguments")
+            raise ValueError("args is required and should be subclass of RuntimeArguments")
 
         if isinstance(args, ImportArguments):
             catalog_runner.run(args, client)
