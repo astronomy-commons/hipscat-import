c6f716640f90ecbbf90a42331b716dc2ae9b31ee
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2023-10-02 09:17:45 -0400

    Verification pipeline boilerplate (#126)
    * Verification pipeline boilerplate
    
    * Input catalog options.
    
    * Contribution docs updates.
    
    * Merge recent changes (#135)
    
    * Use minimum stage name formatting.
    
    * Run copier.
    
    * Add a tad more context for failure email.
    
    * Pin pandas version
    
    * Remove benchmarks for now.
    
    * unpin sphinx versions (#134)
    
    ---------
    
    Co-authored-by: Max West <110124344+maxwest-uw@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Max West <110124344+maxwest-uw@users.noreply.github.com>

---
+++ tests/hipscat_import/verification/test_run_verification.py
@@ -0,0 +1,25 @@
+import pytest
+
+from hipscat_import.verification.arguments import VerificationArguments
+import hipscat_import.verification.run_verification as runner
+
+
+def test_bad_args():
+    """Runner should fail with empty or mis-typed arguments"""
+    with pytest.raises(TypeError, match="VerificationArguments"):
+        runner.run(None)
+
+    args = {"output_catalog_name": "bad_arg_type"}
+    with pytest.raises(TypeError, match="VerificationArguments"):
+        runner.run(args)
+
+
+def test_no_implementation(tmp_path, small_sky_object_catalog):
+    """Womp womp. Test that we don't have a verification pipeline implemented"""
+    args = VerificationArguments(
+        input_catalog_path=small_sky_object_catalog,
+        output_path=tmp_path,
+        output_catalog_name="small_sky_object_verification_report",
+    )
+    with pytest.raises(NotImplementedError, match="not yet implemented"):
+        runner.run(args)
