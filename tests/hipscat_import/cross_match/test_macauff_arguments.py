87e484e8699e7152186192139693acb33bdd25cb
Max West <maxwest@uw.edu>
2023-10-25 15:55:05 -0400

    add more tests for missing coverage

--- tests/hipscat_import/cross_match/test_macauff_arguments.py
+++ tests/hipscat_import/cross_match/test_macauff_arguments.py
@@ -279,3 +279,76 @@ def test_macauff_args_no_metadata(
             input_path=small_sky_dir,
             input_format="csv",
         )
+
+def test_macauff_args_invalid_metadata_file(
+        small_sky_object_catalog,
+        small_sky_source_catalog,
+        small_sky_dir,
+        tmp_path
+):
+    with pytest.raises(ValueError, match="column metadata file must"):
+        args = MacauffArguments(
+            output_path=tmp_path,
+            output_catalog_name="object_to_source",
+            tmp_dir=tmp_path,
+            left_catalog_dir=small_sky_object_catalog,
+            left_ra_column="ra",
+            left_dec_column="dec",
+            left_id_column="id",
+            right_catalog_dir=small_sky_source_catalog,
+            right_ra_column="source_ra",
+            right_dec_column="source_dec",
+            right_id_column="source_id",
+            input_path=small_sky_dir,
+            input_format="csv",
+            metadata_file_path="ceci_n_est_pas_un_fichier.xml",
+        )
+
+def test_macauff_args_invalid_input_directory(
+        small_sky_object_catalog,
+        small_sky_source_catalog,
+        formats_yaml,
+        tmp_path
+):
+    with pytest.raises(FileNotFoundError, match="input_path not found"):
+        args = MacauffArguments(
+            output_path=tmp_path,
+            output_catalog_name="object_to_source",
+            tmp_dir=tmp_path,
+            left_catalog_dir=small_sky_object_catalog,
+            left_ra_column="ra",
+            left_dec_column="dec",
+            left_id_column="id",
+            right_catalog_dir=small_sky_source_catalog,
+            right_ra_column="source_ra",
+            right_dec_column="source_dec",
+            right_id_column="source_id",
+            input_path="ceci_n_est_pas_un_directoire/",
+            input_format="csv",
+            metadata_file_path=formats_yaml,
+        )
+
+def test_macauff_args_no_files(
+        small_sky_object_catalog,
+        small_sky_source_catalog,
+        small_sky_dir,
+        formats_yaml,
+        tmp_path
+):
+    with pytest.raises(FileNotFoundError, match="No input files found"):
+        args = MacauffArguments(
+            output_path=tmp_path,
+            output_catalog_name="object_to_source",
+            tmp_dir=tmp_path,
+            left_catalog_dir=small_sky_object_catalog,
+            left_ra_column="ra",
+            left_dec_column="dec",
+            left_id_column="id",
+            right_catalog_dir=small_sky_source_catalog,
+            right_ra_column="source_ra",
+            right_dec_column="source_dec",
+            right_id_column="source_id",
+            input_path=small_sky_dir,
+            input_format="parquet", # no files of this format will be found
+            metadata_file_path=formats_yaml,
+        )
