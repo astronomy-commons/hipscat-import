70d388f14699b3c7946019c48156668093671379
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-03-14 16:44:18 -0400

    change sphinx theme to book theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,11 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 
 # This assumes that sphinx-build is called from the root directory
 master_doc = "index"
-html_theme = "sphinx_rtd_theme"
+
+html_theme = "sphinx_book_theme"
+
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 extensions = [
     "sphinx.ext.mathjax",
